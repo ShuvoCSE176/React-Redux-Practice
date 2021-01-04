0304c4a0bba6ad3c6b39c9edf439ecafdfa154fd
ShuvoCSE176 <asifshuvo119181@gmail.com>
2021-01-04 19:18:39 +0600

    first commit

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Products = (props) => {
+    const {name,id}=props.product;
+    return (
+        <div style={{border: '1px solid black',padding: '5px'}}>
+            <h2>{name}</h2>
+            <button>Add to cart</button>
+            
+        </div>
+    );
+};
+
+export default Products;
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Products from '../Products/Products';
+
+const Shop = () => {
+
+    const products=[
+
+        {name:'Lenovo Laptop', id:1},
+        {name:'Toshiba Laptop', id:2},
+        {name:'Dell Laptop', id:3},
+        {name:'Doyle Laptop', id:4},
+        {name:'Panasonic Laptop', id:5},
+        {name:'Maximus Laptop', id:6},
+        
+    ]
+    return (
+        <div>
+            <h4>This is shop</h4>
+
+            {
+                products.map(pd =><Products product={pd}></Products>)
+            }
+            
+        </div>
+    );
+};
+
+export default Shop;
