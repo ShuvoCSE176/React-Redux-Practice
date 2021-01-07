f37251b426a09d87397e3c28f4d27984c6a7be9b
ShuvoCSE176 <asifshuvo119181@gmail.com>
2021-01-07 19:07:54 +0600

    last

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeFromCart } from '../../Redux/actions/cartActions';
 
-const Cart = () => {
+const Cart = (props) => {
+    const { cart, removeFromCart } = props
     return (
         <div>
             <h3>This is cart</h3>
+
+            {
+                cart.map(id=><li>{id}<button onClick={()=>removeFromCart(id)}>X</button></li>)
+
+            }
         </div>
+
+
     );
 };
 
-export default Cart;
+const mapStateToProps = (state) => {
+    return {
+        cart: state.cart
+    }
+}
+
+const mapDispatchToProps = {
+    removeFromCart: removeFromCart
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- src/Redux/reducers/cartReducers.js
+++ src/Redux/reducers/cartReducers.js
@@ -17,14 +17,14 @@ const cartReducers = (state = initialState, action) => {
             const newId = action.id;
             const newCart = [...state.cart, newId];
 
-            return {products:state.products, cart: newCart }
+            return {...state, cart: newCart }
 
 
         case REMOVE_FROM_CART:
             const id = action.id;
             const remainingCart = state.cart.filter(item => item !== id);
 
-            return { cart: remainingCart }
+            return {...state, cart: remainingCart }
 
         default: return state;
     }
